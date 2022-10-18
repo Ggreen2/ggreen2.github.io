32edced770e54d33c15acc5995a8c5334ee36ad5
Garrett Greenfield <gjg@iastate.edu>
2022-10-18 16:37:03 -0400

    Updates

---
+++ precache-manifest.4845d5f70fe7ca06a6338d2d7c987196.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d18c0cf46d80d56bb93",
+    "url": "/static/css/main.16258aad.chunk.css"
+  },
+  {
+    "revision": "3d18c0cf46d80d56bb93",
+    "url": "/static/js/main.3d18c0cf.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "fe9e49dcc9c458a203f8",
+    "url": "/static/js/2.fe9e49dc.chunk.js"
+  },
+  {
+    "revision": "f8403e306dc7e4031e5b",
+    "url": "/static/js/runtime~main.f8403e30.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "22fb732a462e25f75cc2cb363cf7ff8b",
+    "url": "/index.html"
+  }
+];
